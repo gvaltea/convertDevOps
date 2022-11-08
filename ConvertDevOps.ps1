1c76e9aa308d90b616360d6b402805d04cdb8732
gvaltea <75476438+gvaltea@users.noreply.github.com>
2022-11-08 17:15:55 +0100

    Add files via upload

---
+++ ConvertDevOps.ps1
@@ -0,0 +1,19 @@
+﻿$NID = Import-Csv -Delimiter "`t" -Path '.\Copia di Stime_NID.txt' -Encoding utf8
+
+$issues = @()
+
+$NID | foreach {
+    $issue =[PSCustomObject]@{      "Work Item Type" = 'Issue'
+                                    "Issue ID" = $_.NID
+                                    Area = $_.AREA
+                                    Title = $_.Titolo
+                                    Description = '<p>' + $_.Descrizione + '</p><p>' + $_.Note + '<p>'
+                                    Effort = $_.GG
+
+                                }
+    $issues += @($issue)
+    
+}
+
+
+$issues | Export-Csv -Path Stime.csv -Encoding UTF8
